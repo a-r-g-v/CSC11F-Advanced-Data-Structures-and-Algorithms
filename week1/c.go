97b5f8ba1f63ad51b0d515f9b21837ef0e122d51
Yuki Mukasa <info@arg.vc>
2018-04-16 03:14:05 +0900

    submit

--- week1/c.go
+++ week1/c.go
@@ -1,35 +1,34 @@
 package main
 
 import (
+	"bufio"
 	"container/heap"
 	"fmt"
+	"os"
+	"strconv"
 )
 
-const INF = 9999999999999999
+var INF int = 1 << 31
 
-type node struct {
-	me    int
+type pair struct {
 	total int
-	index int
+	next  int
 }
 
 func (pq PriorityQueue) Swap(i, j int) {
 	pq[i], pq[j] = pq[j], pq[i]
-	pq[i].index = i
-	pq[j].index = j
 }
 
-type PriorityQueue []*node
+type PriorityQueue []pair
 
 func (pq PriorityQueue) Len() int { return len(pq) }
 
 func (pq PriorityQueue) Less(i, j int) bool {
-	return pq[i].total < pq[j].total
+	return pq[i].total <= pq[j].total
 }
 
 func (pq *PriorityQueue) Push(x interface{}) {
-	item := x.(*node)
-	item.index = len(*pq)
+	item := x.(pair)
 	*pq = append(*pq, item)
 }
 
@@ -37,7 +36,6 @@ func (pq *PriorityQueue) Pop() interface{} {
 	old := *pq
 	n := len(old)
 	item := old[n-1]
-	item.index = -1
 	*pq = old[0 : n-1]
 	return item
 }
@@ -47,62 +45,62 @@ type edge struct {
 	cost int
 }
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func next() int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return i
+
+}
+
 func main() {
+	sc.Split(bufio.ScanWords)
+
 	var V, E, r int
 	fmt.Scanf("%d %d %d", &V, &E, &r)
 
-	edges := make([][]edge, V)
-	nodes := make([]*node, V)
-	queue := make(PriorityQueue, V)
+	edges := make([][]*edge, V)
+	dist := make([]int, V)
+	done := make([]bool, V)
+	queue := make(PriorityQueue, 0, V)
 
 	for i := 0; i < E; i++ {
 		var s, t, cost int
-		n, _ := fmt.Scanf("%d %d %d", &s, &t, &cost)
-		if n != 3 {
-			panic(fmt.Sprintf("scaned string want 3 but %d", n))
-		}
-		edges[s] = append(edges[s], edge{to: t, cost: cost})
+		s, t, cost = next(), next(), next()
+		edges[s] = append(edges[s], &edge{to: t, cost: cost})
 
 	}
 
 	for i := 0; i < V; i++ {
-		if r == i {
-			nodes[i] = &node{me: i, total: 0}
-			queue[i] = nodes[i]
-		} else {
-			nodes[i] = &node{me: i, total: INF}
-			queue[i] = nodes[i]
-
-		}
+		dist[i] = INF
 	}
-	heap.Init(&queue)
+
+	heap.Push(&queue, pair{total: 0, next: r})
 
 	for queue.Len() > 0 {
+		item := heap.Pop(&queue).(pair)
+		if done[item.next] {
+			continue
+		}
+		done[item.next] = true
+		dist[item.next] = item.total
 
-		node := heap.Pop(&queue).(*node)
-
-		for _, edge := range edges[node.me] {
-			if nodes[edge.to].index == -1 {
-				continue
-
-			}
-			alt := edge.cost + node.total
-
-			if alt < nodes[edge.to].total {
-				nodes[edge.to].total = alt
-				heap.Fix(&queue, nodes[edge.to].index)
-
-			}
+		for _, edge := range edges[item.next] {
+			heap.Push(&queue, pair{
+				total: edge.cost + item.total,
+				next:  edge.to,
+			})
 
 		}
 
 	}
 
-	for _, node := range nodes {
-		if node.total == INF {
+	for _, node := range dist {
+		if node >= INF {
 			fmt.Printf("INF\n")
 		} else {
-			fmt.Printf("%d\n", node.total)
+			fmt.Printf("%d\n", node)
 		}
 	}
 
